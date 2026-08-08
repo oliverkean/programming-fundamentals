51706d0c540d6e2c9f19334eac7591c19a2aa802
oliverkean <oliverpalgue@gmail.com>
2026-08-07 22:31:39 -0400

    comments in python

---
+++ comments.py
@@ -0,0 +1,7 @@
+#this is a comment
+# to comment use hash
+# comment your intent to code
+# pseudocode is a way to plan your code before you write it
+"""
+multipline comments are done with triple quotes
+"""
